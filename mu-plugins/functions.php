2728e375d29d7e5278949f89c7e2f999c74872d9
Abdou00 <abdoulaye_diarra@outlook.fr>
2016-10-11 17:08:29 +0200

    Ajout du custom post partenaire

--- mu-plugins/functions.php
+++ mu-plugins/functions.php
@@ -47,7 +47,7 @@ function ressources() {
     'labels'          => $labels,
     'menu_position'   => 7,
     'capability_type' => 'post',// Utilise les mêmes permissions que pour les articles.
-    'taxonomies'      => array('wpsclocation', 'post_tag'),
+    'taxonomies'      => array('category', 'post_tag'),
     'supports'        => array('title','editor','author','wpsclocation','comments','thumbnail')
   ));
 }
@@ -59,34 +59,35 @@ add_action('init', 'events');
 function events()
 {
 	$labels = array(
-		'name'					=> _x( 'Calendrier des événements', 'post type general name' ),
-		'singular_name'			=> _x( 'Event', 'post type singular name' ),
-		'add_new'				=> _x( 'Ajouter', 'events' ),
-		'add_new_item'			=> __( 'Ajouter Événements' ),
-		'edit_item'				=> __( 'Editer Événement' ),
-		'new_item'				=> __( 'Nouveau' ),
-		'all_items'				=> __( 'Tous les Événement' ),
-		'view_item'				=> __( 'Voir Événement' ),
-		'search_items'			=> __( 'Chercher Événements' ),
-		'not_found'				=>  __( 'Aucun Événements Trouvé' ),
+		'name'								=> _x( 'Evénements', 'post type general name' ),
+		'singular_name'				=> _x( 'Event', 'post type singular name' ),
+		'add_new'							=> _x( 'Ajouter', 'events' ),
+		'add_new_item'				=> __( 'Ajouter Événements' ),
+		'edit_item'						=> __( 'Editer Événement' ),
+		'new_item'						=> __( 'Nouveau' ),
+		'all_items'						=> __( 'Tous les Événement' ),
+		'view_item'						=> __( 'Voir Événement' ),
+		'search_items'				=> __( 'Chercher Événements' ),
+		'not_found'						=>  __( 'Aucun Événements Trouvé' ),
 		'not_found_in_trash'	=> __( 'Aucun Événements Trouvé Dans La Corbeille' ),
 		'parent_item_colon'		=> '',
-		'menu_name'				=> 'Événements'
+		'menu_name'						=> 'Événements'
 	);
 
 	$args = array(
 		'labels' => $labels,
 		'public' => true,
 		'publicly_queryable' => true,
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'query_var' => true,
-		'rewrite' => false,
-		'capability_type' => 'post',
-		'has_archive' => true,
-		'hierarchical' => false,
-		'menu_position' => 6,
-		'supports' => array('title','editor','thumbnail')
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => false,
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 6,
+		'taxonomies'         => array('wpsclocation','category'),
+		'supports'           => array('title','editor','thumbnail')
 	);
 
 	register_post_type('events',$args);
@@ -94,34 +95,31 @@ function events()
 
 add_action('init', 'events_taxonomies', 0);
 function events_taxonomies(){
-	// Setup taxonomy for location
 	$labels = array(
 		'name'					=> _x( 'Lieu', 'taxonomy general name' ),
-		'singular_name'			=> _x( 'Location', 'taxonomy singular name' ),
-		'search_items'			=> __( 'Chercher lieu' ),
-		'all_items'				=> __( 'Tous les lieux' ),
-		'edit_item'				=> __( 'Editer Lieu' ),
-		'update_item'			=> __( 'Update Location' ),
-		'add_new_item'			=> __( 'Ajouter' ),
-		'new_item_name'			=> __( 'New Location Name' ),
-		'menu_name'				=> __( 'Lieux' )
+		'singular_name'	=> _x( 'Location', 'taxonomy singular name' ),
+		'search_items'	=> __( 'Chercher lieu' ),
+		'all_items'			=> __( 'Tous les lieux' ),
+		'edit_item'			=> __( 'Editer Lieu' ),
+		'update_item'		=> __( 'Update Location' ),
+		'add_new_item'	=> __( 'Ajouter' ),
+		'new_item_name'	=> __( 'New Location Name' ),
+		'menu_name'			=> __( 'Lieux' )
 	);
 
 	$args = array(
-		'hierarchical'				=> true,
-		'labels'					=> $labels,
-		'show_ui'					=> true,
-		'show_admin_column'			=> true,
+		'hierarchical'			=> true,
+		'labels'						=> $labels,
+		'show_ui'						=> true,
+		'show_admin_column'	=> true,
 		'query_var'					=> true,
-		'rewrite'					=> false,
-    );
-
-	register_taxonomy( 'wpsclocation', array( 'events' ), $args );
+		'rewrite'						=> false,
+  );
+	register_taxonomy('wpsclocation', array('events'), $args);
 }
 
 add_filter( 'meta_boxes', 'events_metaboxes' );
 function events_metaboxes( array $meta_boxes ) {
-
 	// Start with an underscore to hide fields from custom fields list
 	$prefix = 'wpsc_';
 
@@ -146,17 +144,17 @@ function events_metaboxes( array $meta_boxes ) {
 				'type' => 'text_date',
 			),
 			array(
-	            'name' => 'Commence à:',
-	            'desc' => '',
-	            'id'   => $prefix . 'start_time',
-	            'type' => 'text_time',
-	        ),
+	      'name' => 'Commence à:',
+	      'desc' => '',
+	      'id'   => $prefix . 'start_time',
+	      'type' => 'text_time',
+	    ),
 			array(
-	            'name' => 'Fini à:',
-	            'desc' => '',
-	            'id'   => $prefix . 'end_time',
-	            'type' => 'text_time',
-	        ),
+	      'name' => 'Fini à:',
+	      'desc' => '',
+	      'id'   => $prefix . 'end_time',
+	      'type' => 'text_time',
+	    ),
 			array(
 				'name' => 'Inscription URL',
 				'desc' => 'URL complète, y compris http://',
@@ -171,9 +169,7 @@ function events_metaboxes( array $meta_boxes ) {
 			),
 		),
 	);
-
 	// Add other metaboxes as needed
-
 	return $meta_boxes;
 }
 
@@ -185,68 +181,144 @@ function cmb_initialize_cmb_meta_boxes() {
 
 }
 /*************************************************
+    Création du Custom Post(Réseaux Impliqués)
+*************************************************/
+add_action('init', 'partners');
+function partners(){
+	$labels = array(
+		'name'								=> _x( 'Réseaux Impliqués', 'post type general name' ),
+		'singular_name'				=> _x( 'Réseau Impliqué', 'post type singular name' ),
+		'add_new'							=> _x( 'Ajouter', 'Réseaux Impliqués' ),
+		'add_new_item'				=> __( 'Ajouter des Réseaux Impliqués' ),
+		'edit_item'						=> __( 'Editer Réseau Impliqué' ),
+		'new_item'						=> __( 'Nouveau' ),
+		'all_items'						=> __( 'Tous les Réseaux Impliqués' ),
+		'view_item'						=> __( 'Voir Réseau Impliqué' ),
+		'search_items'				=> __( 'Chercher Réseaux Impliqués' ),
+		'not_found'						=>  __( 'Aucun Réseaux Impliqués Trouvé' ),
+		'not_found_in_trash'	=> __( 'Aucun Réseaux Impliqués Trouvé Dans La Corbeille' ),
+		'parent_item_colon'		=> '',
+		'menu_name'						=> 'Réseaux Impliqués'
+	);
+	$args = array(
+		'labels' 							=> $labels,
+		'public' 							=> true,
+		'publicly_queryable' 	=> true,
+		'show_ui' 						=> true,
+		'show_in_menu' 				=> true,
+		'query_var' 					=> true,
+		'rewrite' 						=> false,
+		'capability_type' 		=> 'post',
+		'has_archive' 				=> true,
+		'hierarchical' 				=> false,
+		'menu_position' 			=> 6,
+		'supports' 						=> array('title','editor','thumbnail')
+	);
+	register_post_type('partenaires',$args);
+}
+
+add_filter( 'meta_boxes', 'partners_metaboxes' );
+function partners_metaboxes( array $meta_boxes ) {
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = 'part_';
+	$meta_boxes[] = array(
+		'id'         => 'partners_meta',
+		'title'      => 'Lien du Site',
+		'pages'      => array( 'partenaires', ), // Post type
+		'context'    => 'normal',
+		'priority'   => 'high',
+		'show_names' => true, // Show field names on the left
+		'fields'     => array(
+			array(
+				'name' => 'Lien du Site',
+				'desc' => '',
+				'id'   => $prefix . 'lien_du_site',
+				'type' => 'text_date',
+			),
+		),
+	);
+	// Add other metaboxes as needed
+	return $meta_boxes;
+}
+/*************************************************
+                Query page auteur.
+*************************************************/
+function my_post_queries( $query ) {
+    // vérifier qu'on n'est pas sur une page admin
+    if ( !is_admin() && $query->is_main_query() ) {
+        if ( is_author() ) {
+            // montrer tous les articles
+            $query->set( 'posts_per_page', -1 );
+            $query->set( 'post_type', array( 'post' ) );
+        }
+    }
+}
+add_action( 'pre_get_posts', 'my_post_queries' );
+/*************************************************
                 Galerie Photo.
 *************************************************/
 add_shortcode( 'slider-post', 'slider_post_shortcode' );
 function slider_post_shortcode( $attr ) {
-	// On valide la déclaration orderby pour pouvoir l'utiliser dans le shortcode
- 	if ( isset( $attr['orderby'] ) ) {
- 		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
- 		if ( !$attr['orderby'] )
- 		unset( $attr['orderby'] );
- 	}
 
-	// On définit les attributs du shortcode
- 	extract( shortcode_atts( array(
- 		'order' => 'ASC',
- 		'orderby' => 'menu_order ID', //permet de modifier l'ordre désiré dans le gestionnaire des médias
- 		'id' => get_the_ID(),
- 		'size' => 'slider-post', // taille définit au départ les tailles personnalisées que vous avez mises en place avec la fonction add_image_size
- 		'itemtag' => 'li', // on travaille en ul dans ce cas
- 		'include' => '',
- 		'exclude' => get_post_thumbnail_id() //commenter cette ligne (attention à la virgule finale) si vous désirez exclure l'image à la une de la galerie
- 	), $attr ) );
+// On valide la déclaration orderby pour pouvoir l'utiliser dans le shortcode
+ if ( isset( $attr['orderby'] ) ) {
+ $attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+ if ( !$attr['orderby'] )
+ unset( $attr['orderby'] );
+ }
 
-	$id = (int) $id;
-	$orderby = ( 'RAND' == $order ) ? 'none' : $orderby;
+// On définit les attributs du shortcode
+ extract( shortcode_atts( array(
+ 'order' => 'ASC',
+ 'orderby' => 'menu_order ID', //permet de modifier l'ordre désiré dans le gestionnaire des médias
+ 'id' => get_the_ID(),
+ 'size' => 'slider-post', // taille définit au départ les tailles personnalisées que vous avez mises en place avec la fonction add_image_size
+ 'itemtag' => 'li', // on travaille en ul dans ce cas
+ 'include' => '',
+ 'exclude' => get_post_thumbnail_id() //commenter cette ligne (attention à la virgule finale) si vous désirez exclure l'image à la une de la galerie
+ ), $attr ) );
 
- 	if ( ! empty( $include ) ) {
- 		$include = preg_replace( '/[^0-9,]+/', '', $include );
- 		$_attachments = get_posts( array( 'include' => $include, 'post_status' => '', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
- 		$attachments = array();
- 		foreach ( $_attachments as $key => $val ) {
- 			$attachments[$val->ID] = $_attachments[$key];
- 		}
- 	} elseif ( ! empty( $exclude ) ) {
- 		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
- 		$attachments = get_children( array( 'post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
- 	} else {
- 		$attachments = get_children( array( 'post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
- 	}
- 	if ( empty( $attachments ) )
- 	return '';
- 	if ( is_feed() ) {
- 		$output = "\n";
- 		foreach ( $attachments as $att_id => $attachment )
- 		$output .= wp_get_attachment_link( $att_id, 'thumbnail', true ) . "\n";
- 		return $output;
- 	}
+$id = (int) $id;
+$orderby = ( 'RAND' == $order ) ? 'none' : $orderby;
 
- 	$i = 1;
- 	$output = '<div class="flexslider sliderpost"><ul class="slides">';
- 	foreach ( $attachments as $id => $attachment ) {
- 		$full_image_src = wp_get_attachment_image_src( $id, 'full' );
- 		$image_src = wp_get_attachment_image_src( $id, 'postlist' );
- 		$alt = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
- 		$image_title = $attachment->post_title;
- 		$post_id = get_the_ID();
-		$output .= '<li>';
- 		$output .= '<a href="' . esc_url( $full_image_src[0] ) . '" class="lightbox" rel="lightbox'.$post_id.'"  title="'. $image_title .'"><img class="img-' . $i . '" src="' . esc_url( $image_src[0] ) . '" alt="'. $image_title .'" /></a>';
- 		$output .= '<div class="texte"><span>' .$image_title.'</span></div>';
- 		$output .= '</li>';
- 		$i++;
- 	}
+ if ( ! empty( $include ) ) {
+ $include = preg_replace( '/[^0-9,]+/', '', $include );
+ $_attachments = get_posts( array( 'include' => $include, 'post_status' => '', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+ $attachments = array();
+ foreach ( $_attachments as $key => $val ) {
+ $attachments[$val->ID] = $_attachments[$key];
+ }
+ } elseif ( ! empty( $exclude ) ) {
+ $exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+ $attachments = get_children( array( 'post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+ } else {
+ $attachments = get_children( array( 'post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+ }
+ if ( empty( $attachments ) )
+ return '';
+ if ( is_feed() ) {
+ $output = "\n";
+ foreach ( $attachments as $att_id => $attachment )
+ $output .= wp_get_attachment_link( $att_id, 'thumbnail', true ) . "\n";
+ return $output;
+ }
 
-	$output .= '</ul></div>';
- 	return $output;
-}
+ $i = 1;
+ $output = '<div class="flexslider sliderpost"><ul class="slides">';
+ foreach ( $attachments as $id => $attachment ) {
+ $full_image_src = wp_get_attachment_image_src( $id, 'full' );
+ $image_src = wp_get_attachment_image_src( $id, 'postlist' );
+ $alt = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
+ $image_title = $attachment->post_title;
+ $post_id = get_the_ID();
+
+$output .= '<li>';
+ $output .= '<a href="' . esc_url( $full_image_src[0] ) . '" class="lightbox" rel="lightbox'.$post_id.'"  title="'. $image_title .'"><img class="img-' . $i . '" src="' . esc_url( $image_src[0] ) . '" alt="'. $image_title .'" /></a>';
+ $output .= '<div class="texte"><span>' .$image_title.'</span></div>';
+ $output .= '</li>';
+ $i++;
+ }
+
+$output .= '</ul></div>';
+ return $output;
+ }
