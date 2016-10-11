e63e3f3c0d8ea6d080805b81639ec8279727dda8
Abdou00 <abdoulaye_diarra@outlook.fr>
2016-10-11 16:10:00 +0200

    Boucle de récupération des lieux

--- themes/bakedwp/page-events.php
+++ themes/bakedwp/page-events.php
@@ -15,20 +15,43 @@ Template Name: Événements
 
 			<div id="content">
 				<div id="inner-content" class="row">
-				    <div id="main" class="large-10 medium-10 small-centered columns" role="main">
-					      <?php
-                $args = array( 'post_type' => 'events', 'posts_per_page' => 10 );
-                $loop = new WP_Query( $args );
-                while ( $loop->have_posts() ) : $loop->the_post();
-                  the_title();
-                  echo '<div class="entry-content">';
-									echo '<div class="">';
-										the_post_thumbnail();
-									echo '</div>';
-                    the_content();
-                  echo '</div>';
-                endwhile;
-					    	?>
+				    <div id="main" class="large-12 medium-10 small-centered columns" role="main">
+							<div class="columns">
+								<div class="row">
+								  <div class="small-6 columns">
+										<?php the_title('<h3>','</h3>'); ?>
+										<?php
+		                $args = array( 'post_type'=>'events','category_name'=>'Actions','posts_per_page'=> 10);
+		                $loop = new WP_Query( $args );
+		                while ( $loop->have_posts() ) : $loop->the_post();
+		                  the_title();
+		                  echo '<div class="entry-content">';
+											echo '<div class="">';
+												the_post_thumbnail();
+											echo '</div>';
+		                    the_content();
+		                  echo '</div>';
+		                endwhile;
+							    	?>
+									</div>
+								  <div class="small-6 columns">
+										<?php the_title('<h3>','</h3>'); ?>
+										<?php
+										$args = array( 'post_type'=>'events','category_name'=>'Communiqués','posts_per_page'=> 10);
+		                $loop = new WP_Query( $args );
+		                while ( $loop->have_posts() ) : $loop->the_post();
+		                  the_title();
+		                  echo '<div class="entry-content">';
+											echo '<div class="">';
+												the_post_thumbnail();
+											echo '</div>';
+		                    the_content();
+		                  echo '</div>';
+		                endwhile;
+										?>
+									</div>
+								</div>
+							</div>
     				</div> <!-- end #main -->
 				</div> <!-- end #inner-content -->
 			</div> <!-- end #content -->
