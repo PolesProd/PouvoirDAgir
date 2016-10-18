f2db95d9b2c465e22e939e4af2031c3791d4a19d
Etudiant <Etudiant>
2016-10-18 12:41:59 +0200

    repair project and new base

---
+++ Maquette/header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation Starter Template</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="assets/foundation-icons/foundation-icons.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+  </head>
+  <body>
+    <div class="off-canvas-wrapper">
+    <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper >
+
+      <!-- Menu en responsive -->
+      <div class="off-canvas position-left" id="offCanvas" data-off-canvas data-options data-force-top="true">
+
+        <!-- Close button -->
+        <button class="close-button" aria-label="Close menu" type="button" data-close>
+          <span aria-hidden="true">&times;</span>
+        </button>
+
+        <!-- Menu masqué -->
+        <ul class="vertical menu">
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Evenements</a></li>
+          <li><a href="#">ressources</a></li>
+          <li><a href="#">Le collectif</a></li>
+          <li><a href="#">Adhérer</a></li>
+        </ul>
+      </div>
+      <!-- Page content -->
+
+      <!-- menu off canvas small-screen -->
+      <div class="title-bar hide-for-medium">
+        <div class="title-bar-left">
+          <!-- Bouton d'action pour le menu off canvas -->
+          <button type="button" class="menu-icon " data-toggle="offCanvas"></button>
+        </div>
+        <a href="index.php"><img role="logo" class="thumbnail" src="assets/img/logoPVA.png"></a>
+        <div class="title-bar-right">
+          <a role="button" class="button" href="#"><i class="fi-plus"></i></a><a role="button" class="button" href="#" ><i class="fi-link"></i></a><a role="button" class="button" href="#"><i class="fi-magnifying-glass"></i></a>
+        </div>
+
+      </div>
+      <!-- menu off canvas medium-screen -->
---
+++ themes/bakedwp/functions.php
@@ -0,0 +1,39 @@
+<?php
+// Theme support options
+require_once(get_template_directory().'/assets/functions/theme-support.php');
+
+// WP Head and other cleanup functions
+require_once(get_template_directory().'/assets/functions/cleanup.php');
+
+// Register scripts and stylesheets
+require_once(get_template_directory().'/assets/functions/enqueue-scripts.php');
+
+// Register custom menus and menu walkers
+require_once(get_template_directory().'/assets/functions/menu.php');
+require_once(get_template_directory().'/assets/functions/menu-walkers.php');
+
+// Register sidebars/widget areas
+require_once(get_template_directory().'/assets/functions/sidebar.php');
+
+// Makes WordPress comments suck less
+require_once(get_template_directory().'/assets/functions/comments.php');
+
+// Replace 'older/newer' post links with numbered navigation
+require_once(get_template_directory().'/assets/functions/page-navi.php');
+
+// Adds support for multiple languages
+require_once(get_template_directory().'/assets/translation/translation.php');
+
+// Related post function - no need to rely on plugins
+require_once(get_template_directory().'/assets/functions/related-posts.php');
+
+// Customizer support
+require_once(get_template_directory().'/assets/functions/customizer.php');
+
+function callLib(){
+//Call Lib JS (jQuery et jQuery UI)
+    wp_enqueue_script('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js', array('jquery'), '1.8.6');
+    wp_enqueue_script('jquery', get_template_directory().'/assets/js/jquery-3.1.1.min.js');
+}
+add_action( 'wp_enqueue_scripts', 'callLib' );
+?>
