4f51ffb7c586a6c172d876c7b408844e420bbc73
Etudiant <Etudiant>
2016-10-18 16:31:28 +0200

    update et mise en place de la version avant le travail de Salime.

--- themes/bakedwp/assets/functions/menu.php
+++ themes/bakedwp/assets/functions/menu.php
@@ -10,7 +10,7 @@ register_nav_menus(
 function joints_top_nav() {
 	 wp_nav_menu(array(
         'container' => false,                           // Remove nav container
-        'container_class' => '',                        // Class of container
+        'container_class' => '',              // Class of container
         'menu_class' => 'top-bar-menu right',           // Adding custom nav class
         'theme_location' => 'main-nav',                 // Where it's located in the theme
         'before' => '',                                 // Before each link <a>
@@ -18,7 +18,7 @@ function joints_top_nav() {
         'link_before' => '',                            // Before each link text
         'link_after' => '',                             // After each link text
         'depth' => 5,                                   // Limit the depth of the nav
-        'fallback_cb' => 'joints_fallback_menu',                         // Fallback function (see below)
+        'fallback_cb' => 'joints_fallback_menu',        // Fallback function (see below)
         'walker' => new Top_Bar_Walker(),
     ));
 } /* End Top Menu */
--- themes/bakedwp/parts/nav-top-topbar.php
+++ themes/bakedwp/parts/nav-top-topbar.php
@@ -1,17 +1,39 @@
-<div class="fixed contain-to-grid">
+<div class=""> <!-- class="fixed contain-to-grid" -->
 	<nav class="top-bar" data-topbar data-options="scrolltop : false">
-		<ul class="row">
+		<ul class="row text-center">
 			<li class="medium-6 columns"><a href="#">Espace collaboratif</a></li>
+			<?php if( is_user_logged_in() ){
+	       			$current_user = wp_get_current_user();?>
+	      			<li id="" class="menu-item menu-item-type-post_type menu-item-object-page medium-3 columns"><a href="/wp-admin/profile.php"><?php echo $current_user->user_login; ?></a></li><li id="" class="menu-item menu-item-type-post_type menu-item-object-page medium-3 columns"><a href="<?php echo wp_logout_url(); ?>">Déconnexion</a></li><?php
+					  } else {?>
+					      <li id="menu-item-178" class="menu-item menu-item-type-post_type menu-item-object-page medium-6 columns"><a href="/wp-admin/profile.php"><a href="/wp-login.php">connexion</a></li><?php
+					  }
+					?>
 		</ul>
-		<ul class="title-area">
-			<!-- Title Area -->
-			<li class="name">
-				<h1 class="site-title"> <a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
-			</li>
-			<li class="toggle-topbar menu-icon">
-				<a href="#"><span>Menu</span></a>
-			</li>
-		</ul>
+		<section class="row align-top text-center">
+			<!-- partie concernant le log et le titre -->
+			<ul class="title-area medium-6 columns">
+				<!-- Title Area -->
+				<li class="name">
+					<h1 class="site-title"> <a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
+				</li>
+				<li class="menu-text">
+					<h3 class="site-title"><?php bloginfo('description'); ?></h3>
+				</li>
+				<li class="toggle-topbar menu-icon">
+					<a href="#"><span>Menu</span></a>
+				</li>
+			</ul>
+			<ul class="medium-4 columns title-area">
+				<li><a href="<?php echo esc_url(home_url()); ?>/?page_id=5">contact</a></li>
+				<li><a href="https://www.facebook.com/pouvoirdagir/?fref=ts" target="_blank">Facebook</a></li>
+				<li><a href="https://twitter.com/collectif_pa" target="_blank">Twitter</a></li>
+				<li><a href="*">Newsletter</a></li>
+			</ul>
+			<div class="medium-2 columns">
+				<i class="fi-magnifying-glass large"></i>
+			</div>
+		</section>
 		<section class="top-bar-section">
 			<?php joints_top_nav(); ?>
 		</section>
