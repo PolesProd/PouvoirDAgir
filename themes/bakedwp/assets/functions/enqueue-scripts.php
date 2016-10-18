d1b6cdad584a8e8f542c5aea0ac6b9b533880d1b
Abdou00 <abdoulaye_diarra@outlook.fr>
2016-10-18 11:46:18 +0200

    Ajout du calendrier des événements

--- themes/bakedwp/assets/functions/enqueue-scripts.php
+++ themes/bakedwp/assets/functions/enqueue-scripts.php
@@ -5,19 +5,25 @@ function joints_scripts_and_styles() {
 
     // Modernizr from bower_components
     wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/vendor/foundation/js//modernizr-min.js', array(), '2.8.3', true );
-    
+
     // Adding Foundation scripts file in the footer
-   wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/vendor/foundation/js/foundation.min.js', array( 'jquery' ), '', true );
-    
+    wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/vendor/foundation/js/foundation.min.js', array( 'jquery' ), '', true );
+
     // Adding scripts file in the footer
     wp_enqueue_script( 'bakedwo-site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '', true );
-    
+
+    // Adding scripts file in the footer
+    wp_enqueue_script( 'simplecalendar', get_template_directory_uri() . '/assets/js/simplecalendar.js', array( 'jquery' ), '', true );
+
     // Register customized Foundation stylesheets, includes normalize
     wp_enqueue_style( 'foundation', get_template_directory_uri() . '/assets/css/foundation.css', array(), '', 'all' );
-   
+
     // Register main stylesheet
     wp_enqueue_style( 'bakedwp-site-css', get_template_directory_uri() . '/style.css', array(), '', 'all' );
-    
+
+    // Register calendar style
+    wp_enqueue_style( 'calendar', get_template_directory_uri() . '/assets/css/style-personal.css', array(), '', 'all' );
+
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
       wp_enqueue_script( 'comment-reply' );
