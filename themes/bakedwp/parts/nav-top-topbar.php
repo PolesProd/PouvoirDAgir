80bb3757dcee36c7fe635779015a293013b9d008
Etudiant <Etudiant>
2016-10-18 12:45:19 +0200

    modif menu

--- themes/bakedwp/parts/nav-top-topbar.php
+++ themes/bakedwp/parts/nav-top-topbar.php
@@ -1,14 +1,17 @@
 <div class="fixed contain-to-grid">
 	<nav class="top-bar" data-topbar data-options="scrolltop : false">
+		<ul class="row">
+			<li class="medium-6 columns"><a href="#">Espace collaboratif</a></li>
+		</ul>
 		<ul class="title-area">
 			<!-- Title Area -->
 			<li class="name">
-				<h1 class="site-title"> <a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>		
+				<h1 class="site-title"> <a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
 			</li>
 			<li class="toggle-topbar menu-icon">
 				<a href="#"><span>Menu</span></a>
 			</li>
-		</ul>		
+		</ul>
 		<section class="top-bar-section">
 			<?php joints_top_nav(); ?>
 		</section>
