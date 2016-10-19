c8ac7e5e929a92d3a297c24e2f6a32bbfbad3b13
Etudiant <Etudiant>
2016-10-19 12:30:06 +0200

    update du footer. reste quelques problemes de merge

--- themes/bakedwp/footer.php
+++ themes/bakedwp/footer.php
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-<div id="footer-widgets" class="row">
-	<div id="inner-footer-widgers" class="large-10 medium-10 small-centered columns">
-		<div class="row">
-				<?php get_sidebar('sidebar1'); ?>
-				<?php get_sidebar('sidebar2'); ?>
-				<?php get_sidebar('sidebar3'); ?>
-		</div>
-	</div>
-</div>
 
-<footer class="footer row" role="contentinfo">
-			<div class="large-10 medium-10 small-centered columns">
-		<p class="source-org copyright"><?php echo get_theme_mod( 'footer_text', 'BakedWP Theme. Built with <a href="http://jointswp.com">JointsWP.</a>' ); ?></p>
-	</div>
-</footer> <!-- end .footer -->
-</div> <!-- end #container -->
-</div> <!-- end .inner-wrap -->
-</div> <!-- end .off-canvas-wrap -->
-<?php wp_footer(); ?>
-</body>
-=======
 					<div id="footer-widgets" class="row">
 						<div id="inner-footer-widgers" class="large-10 medium-10 small-centered columns">
 							<div class="row">
@@ -32,7 +11,7 @@
 
 					<footer class="footer row" role="contentinfo">
 			        	<div class="large-10 medium-10 small-centered columns">
-							<p class="source-org copyright"><?php echo get_theme_mod( 'footer_text', 'BakedWP Theme. Custom par <a href="http://lepoles.org">LePoleS Prod.</a>' ); ?></p>						
+							<p class="source-org copyright"><?php echo get_theme_mod( 'footer_text', 'BakedWP Theme. Custom par <a href="http://lepoles.org">LePoleS Prod.</a>' ); ?></p>
 						</div>
 					</footer> <!-- end .footer -->
 				</div> <!-- end #container -->
@@ -40,5 +19,4 @@
 		</div> <!-- end .off-canvas-wrap -->
 		<?php wp_footer(); ?>
 	</body>
->>>>>>> e3c70a52925841d0d4fdc9d0a5b313eceab1b9d9
 </html> <!-- end page -->
