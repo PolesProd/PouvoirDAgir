f642968544c8f4af4eee659588adbb29c1c82496
Etudiant <Etudiant>
2016-10-24 18:34:02 +0200

    mise a jour des elements et debut sur le wireframe

--- themes/bakedwp/parts/nav-top-topbar.php
+++ themes/bakedwp/parts/nav-top-topbar.php
@@ -3,22 +3,23 @@
 	<nav class="top-bar" data-topbar data-options="scrolltop : false">
 			<?php if( is_user_logged_in() ){
      			$current_user = wp_get_current_user();?>
-					<ul class="medium-5 medium-offset-7 row text-center">
+					<ul class="medium-4 medium-offset-8 row text-center">
 						<li class="medium-4 columns"><a href="#">Espace collaboratif</a></li>
 	    			<li id="" class="menu-item menu-item-type-post_type menu-item-object-page medium-4 columns"><a href="/wp-admin/profile.php"><?php echo $current_user->user_login; ?></a></li>
 						<li id="" class="menu-item menu-item-type-post_type menu-item-object-page medium-4 columns"><a href="<?php echo wp_logout_url(); ?>">Déconnexion</a></li><?php
 			  } else {?>
-					<ul class="medium-3 medium-offset-9 row text-center">
+					<ul class="medium-2 medium-offset-10 row text-center">
 						<li class="medium-6 columns"><a href="#">Espace collaboratif</a></li>
 			      <li id="menu-item-178" class="menu-item menu-item-type-post_type menu-item-object-page medium-6 columns"><a href="<?php echo get_site_url() . '/wp-login.php'; ?>">connexion</a></li><?php
 			  }?>
 		</ul>
+
 		<section class="expanded row align-top text-center">
 			<!-- partie concernant le log et le titre -->
 			<ul class="title-area medium-6 columns">
 				<!-- Title Area -->
 				<li class="name">
-					<h1 class="site-title"> <a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
+					<h1 class="site-title"> <a href="<?php echo home_url(); ?>" rel="nofollow"><img src="" alt="<?php bloginfo('name'); ?>"/></a></h1>
 				</li>
 				<li class="menu-text">
 					<h3 class="site-title"><?php bloginfo('description'); ?></h3>
