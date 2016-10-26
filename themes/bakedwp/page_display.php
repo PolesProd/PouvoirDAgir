64f6fe957bf7d7d95dcf2eb995450fd7189d4590
Gootime <kevin.cheruel@lepoles.org>
2016-10-26 17:02:45 +0200

    Travil de mercredi broken

--- themes/bakedwp/page_display.php
+++ themes/bakedwp/page_display.php
@@ -1,82 +1,48 @@
 <?php
 /*
-Template Name: Analyse_category
+Template Name: Category_Centrale
 */
 ?>
+<?php get_header();?>
+<h1>Ressources</h1>
+<ul>
+<?php
+$args = array('orderby' => 'name', 'order' => 'ASC', 'fields' => 'all');
+    $url = $_SERVER['REQUEST_URI'];
+    $post_type = get_post_types();
+    $string_arr = implode(',',$post_type);
+    $tab_array = explode(',',$string_arr);
 
-<?php get_header();
-//$post_type = $_POST['post_type'];
-//echo $post_type;
-?>
-  <div class="temoignage" style='border:solid 1px #ccc;'>
-            <?php
-  //Témoignage
-  // $sqlTemoignage = $wpdb->get_results('SELECT * FROM  wp_term_taxonomy WHERE  taxonomy = "'.$post_type.'"');
-  // $countTemoignage = count($sqlTemoignage);
-  // for($i=0;$i <$countTemoignage;$i++){
-    $args = array( 'post_type' => 'analyse',
-      'posts_per_page' => 5,
-    );
-    $loopy = new WP_Query( $args );
-    if($loopy->have_posts()){
-      while ( $loopy->have_posts() ) {
-        $loopy->the_post();
-          echo '<div class="ressoure">';
-            $id = get_the_ID();
-            $author = get_the_author();
-            $terms = wp_get_post_terms($id, 'analyse', $args );
-            $my_date = get_the_date( 'l j F Y' );
-            $myterms = get_terms('analyse', array( 'parent' => 0, 'hide_empty' => 0 ) );
-            echo 'Ressources n<sup>o</sup> '.$id.' appartenant a la catégorie : <strong>'.$terms[0]->name.'</strong> dans <strong>'.$myterms[0]->taxonomy.'</strong><br>';
-            if(has_post_thumbnail()){
-            ?>
-              <div class="imgArticle" id="<?php echo 'post_thumbsnails-'. get_post_thumbnail_id(); ?>" style="font-family: 'Ruda', sans-serif;background-image:url('<?php if ( has_post_thumbnail() ) { echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) );} ;?>');width:300px;height:308px;background-size:cover;background-position: center;background-repeat: no-repeat;">
-              </div><!-- Fin de la div imgArticle --><br><?php
-            }
-            ?>
-            <?php echo 'Date d\'écriture : <strong>'.$my_date.'</strong><br>'; ?>
-            <?php echo 'Par : <strong>'.$author.'</strong><br>'; ?>
-            <?php if(get_post_type($id) === 'analyse'){?>
-              <?php echo 'Lien vers la ressource externe : ';?><strong><a href="<?=implode(get_post_meta(get_the_ID(), 'analyse_externes'))?>" target="_blank"><?=implode(get_post_meta(get_the_ID(), 'analyse_externes'));?></a></strong><br>
-              <?php the_title(); ?><br>
-              <?php echo '<strong><em>'.implode(get_post_meta(get_the_ID(), 'analyse_chapeau')).'</em></strong><br>'; ?>
-              <?php the_excerpt().'<br>';
-              if(is_user_logged_in()){?>
-                <form action="wp-admin/post-new.php?post_type=analyse" method="post">
-                  <input type="submit" value="Créer une Analyse">
-                </form>
-                <?php
-              }
-            }
-              if(get_post_type($id) === 'methodologie'){?>
-              <?php echo 'Lien vers la ressource externe : ';?><strong><a href="<?=implode(get_post_meta(get_the_ID(), 'methodologie_externes'))?>" target="_blank"><?=implode(get_post_meta(get_the_ID(), 'methodologie_externes'));?></a></strong><br>
-              <?php the_title(); ?><br>
-              <?php echo '<strong><em>'.implode(get_post_meta(get_the_ID(), 'methodologie_chapeau')).'</em></strong><br>'; ?>
-              <?php the_excerpt().'<br>';
-              if(is_user_logged_in()){
-                ?>
-                <form action="wp-admin/post-new.php?post_type=methodologie" method="post">
-                  <input type="submit" value="Créer une methodologie">
-                </form>
-                <?php
-              }
-            }
-              if(get_post_type($id) === 'temoignage'){?>
-              <?php echo 'Lien vers la ressource externe : ';?><strong><a href="<?=implode(get_post_meta(get_the_ID(), 'temoignage_externes'))?>" target="_blank"><?=implode(get_post_meta(get_the_ID(), 'temoignage_externes'));?></a></strong><br>
-              <?php the_title(); ?><br>
-              <?php echo '<strong><em>'.implode(get_post_meta(get_the_ID(), 'temoignage_chapeau')).'</em></strong><br>'; ?>
-              <?php the_excerpt().'<br>';
-              if(is_user_logged_in()){
-                ?>
-                <form action="wp-admin/post-new.php?post_type=temoignage" method="post">
-                  <input type="submit" value="Créer une temoignage">
-                </form>
-                <?php
-              }
-            }
-          echo '</div>';
-      }
+
+    foreach($tab_array as $menu){
+      echo '<form action="" method="get">';
+        echo '<input type="hidden" value="'.$menu.'" name="post_type">';
+        echo '<input type="submit" value="'.$menu.'">';
+      echo '</form>';
     }
-  // }?>
+    die();
+    for($i = 0; $i<count($tab_array);$i++){
+    $posts = array();
+    //echo $tab_array[$i];
+      //print_r($tab_array);
+      ?>
+      <?php
+      $post_array = get_posts(array( 'posts_per_page' => -1, 'post_type' => $tab_array[$i]));
+      if(count($post_array) !== 0){
+        echo '<p>il y a '.count($post_array).' article dans '.$tab_array[$i].'</p>';
+      }
+      //print_r($post_array);
+      for($x = 0;$x <count($post_array);$x++){
+            echo '<article>';
+            echo '<p><h2>'.$post_array[$x]->post_title.'</h2></p>';
+            echo '<p>'.$post_array[$x]->post_content.'</p>';
+            echo '</article><hr>';
+      }
 
-</div> <!--Fin Div Méthodologie --><?php get_footer();?>
+    }
+?>
+</ul>
+<?php
+
+ ?>
+<?php get_footer();?>
