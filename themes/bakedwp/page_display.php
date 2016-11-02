def95a3a49590c80ac1ff6d6c959e8c15d0d3ede
Gootime <kevin.cheruel@lepoles.org>
2016-11-02 16:07:21 +0100

    Page Display Debug & Isotope

--- themes/bakedwp/page_display.php
+++ themes/bakedwp/page_display.php
@@ -14,22 +14,17 @@ $tab_taxo = explode(',',$str_taxo);
 $argsTaxo = array(
   'current_category' => 0
 );
-echo wp_list_categories($args);
+
 ?>
 <div class='button-group float-center'>
   <p>
 <button  data-filter="*">Toutes Les Catégories</button><?php
-
-  //print_r($tab_taxo);
-  foreach ( $tab_taxo as $taxonomy ) {
-    if($taxonomy === 'post_tag' || $taxonomy === 'nav_menu' || $taxonomy === 'link_category' || $taxonomy === 'post_format' ){
-    }else{
-     //var_dump($ajax_query);
-     echo '<button  data-filter=".'.$taxonomy.'">'.$taxonomy.'</button>';
-     if($taxonomy !== 'category'){
-       $taxoFirst[] = $taxonomy;
-     }
-   }
+  $terms = get_terms( array(
+    'taxonomy' => array('category','analyse','methodologie','temoignage','wpsclocation','wpsccategory'),
+    'hide_empty' => false,
+) );
+  for($j = 0; $j < sizeof($terms);$j++){
+    echo '<button  data-filter=".'.$terms[$j]->slug.'">'.$terms[$j]->name.'</button>';
   }
   echo '</p></div>';
    ?>
@@ -54,8 +49,13 @@ for($i = 0; $i<count($tab_array);$i++){
 
   $ajax_query = new WP_Query($args);
   if ( $ajax_query->have_posts() ) : while ( $ajax_query->have_posts() ) : $ajax_query->the_post();
+
     if($args['post_type'] === 'page' || $args['post_type'] === 'attachment' || $args['post_type'] === 'revision' || $args['post_type'] === 'nav_menu_item' || $args['post_type'] === 'ressources' || $args['post_type'] === 'wpcf7_contact_form'){
     }else if($args['post_type'] === 'post'){
+      // echo '<p> Type Post :';
+      // print_r($post);
+      // echo '</p>';
+
       $cat = get_the_category($post->ID);?>
        <div class="grid-item <?=$tab_array[$i];?> <?=$cat[0]->category_nicename;?>" data-category='transtition'>
        <h2><?php the_title(); ?></h2>
@@ -64,29 +64,36 @@ for($i = 0; $i<count($tab_array);$i++){
        	<a href="<?php the_permalink(); ?>">Lire</a>
        </div>
      <?php
-      }
+    }
       else{
-        echo count($taxoFirst);
+        $terms = '';
 
-        for($j = 0; $j <count($taxoFirst);$j++){
-          foreach ($taxoFirst as $taxo) {
-              
-              // $terms = get_the_terms( $post->ID, $taxo);
-              // $termID[] = $taxo->term_id;
-              // $termName[] = $taxo->name;
-          }
-      }
-      // echo '<p>'.$termName[0].'</p>';
-      // echo '<p>'.$termID[0].'</p>';
-      //$child = get_term_children($termID[0],$termName[0]);
-        ?>
-          <div class="grid-item <?=$tab_array[$i];?>" data-category='transtition'>
-          <h2><?php the_title(); ?></h2>
-           <?php the_excerpt(); ?>
-           <a href="<?php the_permalink(); ?>">Lire</a>
-          </div>
-        <?php
-      }
+        if($post->post_type == 'events'){
+
+          $termsFirst = wp_get_post_terms( $post->ID, 'wpsccategory');
+          $termsSecond = wp_get_post_terms( $post->ID, 'wpsclocation');
+
+          $terms = $termsFirst[0]->slug . ' ';
+          $terms .= $termsSecond[0]->slug;
+
+        }else if($post->post_type == 'analyse'){
+          $termsFirst = wp_get_post_terms( $post->ID, 'analyse' );
+          $terms = $termsFirst[0]->slug;
+        }else if($post->post_type == 'methodologie'){
+          $termsFirst = wp_get_post_terms( $post->ID, 'methodologie' );
+          $terms = $termsFirst[0]->slug;
+        }else if($post->post_type == 'temoignage'){
+          $termsFirst = wp_get_post_terms( $post->ID, 'temoignage' );
+          $terms = $termsFirst[0]->slug;
+        }
+          ?>
+        
+      <div class="grid-item <?=$tab_array[$i] .' '. $terms;?>" data-category='transtition'>
+      <h2><?php the_title(); ?> </h2>
+       <?php the_excerpt(); ?>
+       <a href="<?php the_permalink(); ?>">Lire</a>
+      </div>
+<?php }
 endwhile;
 endif;
 }
