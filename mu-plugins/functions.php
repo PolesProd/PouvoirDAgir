6baa0f8b8cb4264583b755b332d14ac7328e1e95
Abdou00 <abdoulaye_diarra@outlook.fr>
2016-11-15 12:14:58 +0100

    Test de la fonction n4

--- mu-plugins/functions.php
+++ mu-plugins/functions.php
@@ -4,15 +4,11 @@ Plugin Name: Fonctions WordPress
 Description: L'ensemble des fonctions globales du site.
 Version: 0.1
 License: No fucking licence
-Author: Kevin CHERUEL
+Author: LePoleS Prod
 Author URI: https://www.lepoles.org/
 */
 /********************************************
-<<<<<<< HEAD
     Fonction initialise les Query Posts.
-=======
-		Fonction initialisation les Query Posts.
->>>>>>> Test fonction ajax
 ********************************************/
 add_filter('pre_get_posts', 'query_post_type');
 function query_post_type($query) {
@@ -73,11 +69,11 @@ add_action( 'wp_footer', 'auto_search' );
                 Galerie
 *************************************************/
 function gallery_func(){
-    $argsThumb = array(
-      'order'          => 'ASC',
-      'post_type'      => 'attachment',
-      'post_mime_type' => 'image',
-      'post_status'    => null
+  $argsThumb = array(
+    'order'          => 'ASC',
+    'post_type'      => 'attachment',
+    'post_mime_type' => 'image',
+    'post_status'    => null
   );
   $attachments = get_posts($argsThumb);
   if ($attachments) {
@@ -132,7 +128,6 @@ add_action('pre_get_posts', 'my_post_queries');
 add_action('wp_enqueue_scripts', 'add_js_scripts');
 function add_js_scripts() {
 	wp_enqueue_script( 'script', get_template_directory_uri().'/assets/js/test.js', array('jquery'), '1.0', true );
-  wp_enqueue_script( 'isotope', get_template_directory_uri().'/assets/js/isotope.js', array('jquery'), '1.0', true );
 	// Passer Ajax Url à script.js
 	wp_localize_script('script', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
 }
@@ -151,6 +146,8 @@ function call_events_ajax() {
 		'offset' => $offset
 	);
 	$loop = new WP_Query($args);
+	$i = 0;
+	$file = plugins_url() . '/test.json';
 	$array = array();
 	while ($loop->have_posts()) : $loop->the_post();
 		global $events;
@@ -170,135 +167,58 @@ function call_events_ajax() {
 
 	wp_reset_query();
 	ob_clean();
-  json_encode($array);
-	$test = explode(",", $array);
-	// $uploads = wp_upload_dir();
-	$json = get_template_directory_uri() . '/events.json';
-	if (is_writable($json)) {
-		// if (!$handle = fopen($jsonfile, 'r+')) {
-		// 	echo "Erreur!! Impossible d'ouvrir $jsonfile.";
-		// 	exit;
-		// }
-		$handle = fopen($json, 'r+');
-		if (fwrite($handle, $test) === FALSE) {
-			echo "Erreur !! Impossible d'écrire dans $json.";
-			exit;
-		}
-		echo "Succès !! L'écriture dans $json a réussi.";
-		fclose($handle);
-	} else {
-		echo "$json n'est pas accessible en écriture.";
+	// echo wp_json_encode($array);
+
+	echo "=========================";
+	echo "<br />";
+
+	foreach($array as $i => $value){
+    // print($i .':'. $value .',') . '<br />';
+    // print_r($array[$i]);
+    // echo $array[$i]['day'], $array[$i]['month'], $array[$i]['year'], $array[$i]['title'], $array[$i]['description'] . '<br />';
+    // echo "<br />";
+    // print_r($array[$i]);
+    // echo "<br />";
+    // echo json_encode($array[$i]);
+    // echo "<br />";
+    $string = implode(",", $array[$i]) . '<br/>';
+    echo($string);
 	}
-	echo '<br>';
+  echo "=========================";
+
+  // Assurons nous que le fichier est accessible en écriture
+if (is_writable($file)) {
+    // Dans notre exemple, nous ouvrons le fichier $filename en mode d'ajout
+    // Le pointeur de fichier est placé à la fin du fichier
+    // c'est là que $somecontent sera placé
+    $handle = fopen($file, 'w');
+    // Ecrivons quelque chose dans notre fichier.
+    if (fwrite($handle, $string) === FALSE) {
+        echo "Impossible d'écrire dans le fichier ($file)";
+        exit;
+    }
+
+    echo "L'écriture de ($string) dans le fichier ($file) a réussi";
+
+    fclose($handle);
+
+} else {
+    echo "Le fichier $file n'est pas accessible en écriture.";
 }
-
-function custom_js_to_head() {
-	?>
-	<script>
-	jQuery(function(){
-		var childMenu = jQuery(jQuery(jQuery('#menu-posts-ressources').children())[1]).children()
-		var defType = '';
-		//Change default routing ressources post type
-
-		jQuery('li>.menu-icon-ressources').attr('href','edit.php?post_type=analyse');
-
-		//Append sub menu categories
-		for(var i= 0; i<childMenu.length;i++){
-			if(jQuery(childMenu[i]).hasClass('current') == true){
-				var type = jQuery(jQuery(childMenu[i]).children()[0]).attr('href').split("=")[1]
-				if(type == 'analyse'){
-					jQuery(jQuery(childMenu[i]).children()[0]).append('<ul><li><a href="edit-tags.php?taxonomy=analyse&post_type=analyse" class="page-title-action">Catégorie Analyse</a></li></ul>');
-				}else if( type == 'temoignage' ){
-					jQuery(jQuery(childMenu[i]).children()[0]).append('<ul><li><a href="edit-tags.php?taxonomy=temoignage&post_type=temoignage" class="page-title-action">Catégorie Temoignage</a></li></ul>');
-				}else if ( type == 'methodologie' ){
-					jQuery(jQuery(childMenu[i]).children()[0]).append('<ul><li><a href="edit-tags.php?taxonomy=methodologie&post_type=methodologie" class="page-title-action">Catégorie Methodologie</a></li></ul>');
-				}
-			}
-		}
-	});
-	</script>
-	<?php
-}
-add_action('admin_head', 'custom_js_to_head');
-/*************************************************
-				Query page auteurs.
-*************************************************/
-add_action('pre_get_posts', 'my_post_queries');
-function my_post_queries($query){
-	// vérifier qu'on n'est pas sur une page admin
-	if (!is_admin() && $query->is_main_query()){
-		if (is_author()){
-			// montrer tous les articles
-			$query->set('posts_per_page', -1);
-			$query->set('post_type', array('post'));
-		}
-	}
-}
-/*************************************************
-						Query Ajax.
-*************************************************/
-add_action('wp_enqueue_scripts', 'add_js_scripts');
-function add_js_scripts() {
-	wp_enqueue_script( 'script', get_template_directory_uri().'/assets/js/test.js', array('jquery'), '1.0', true );
-	// Passer Ajax Url à script.js
-	wp_localize_script('script', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
-}
-
-add_action( 'wp_ajax_events_ajax', 'events_ajax' );
-add_action( 'wp_ajax_nopriv_events_ajax', 'events_ajax' );
-function events_ajax() {
-	$offset = $_POST['offset'];
-	$args = array(
-		'post_type' => 'events',
-		'post_status' => 'publish',
-		'posts_per_page' => -1,
-		'meta_key' => 'wpsc_start_date',
-		'offset' => $offset
-	);
-	$loop = new WP_Query($args);
-	$array = array();
-	while ($loop->have_posts()) : $loop->the_post();
-		global $events;
-		$date = get_post_meta(get_the_ID(), 'wpsc_start_date', true);
-		$result = explode("/", $date);
-		$day = $result[1];
-		$month = $result[0];
-		$year = $result[2];
-		$array[] = array(
-			'day'=> $day,
-			'month'=> $month,
-			'year'=> $year,
-			'title' => get_the_title(),
-			'description' => get_the_excerpt($post),
-		);
-	endwhile;
-
-	wp_reset_query();
-	ob_clean();
-	json_encode($array);
-	$test = explode(",", $array);
-	// $uploads = wp_upload_dir();
-	$json = esc_url($uploads['baseurl'] . '/events.json');
-	// $json = get_template_directory_uri() . '/events.json';
-	if (is_writable($json)) {
-		// if (!$handle = fopen($jsonfile, 'r+')) {
-		// 	echo "Erreur!! Impossible d'ouvrir $jsonfile.";
-		// 	exit;
-		// }
-		$handle = fopen($json, 'r+');
-		if (fwrite($handle, $test) === FALSE) {
-			echo "Erreur !! Impossible d'écrire dans $json.";
-			exit;
-		}
-		echo "Succès !! L'écriture dans $json a réussi.";
-		fclose($handle);
-	} else {
-		echo "$json n'est pas accessible en écriture.";
-	}
-	echo '<br>';
+	// if (is_writable($file)) {
+	//  	$write = file_get_contents($file);
+	//  	$write .= $string;
+	//  	// FILE_APPEND pour rajouter à la suite du fichier et
+	//  	// LOCK_EX pour empêcher quiconque d'autre d'écrire dans le fichier en même temps
+	//  	file_put_contents($file, $write, FILE_APPEND | LOCK_EX);
+	//  		echo "Erreur !! Impossible d'écrire dans $file.";
+	//  		exit;
+	//  	echo "Succès !! L'écriture dans $file a réussi.";
+	// } else {
+	//  	echo "$file n'est pas accessible en écriture.";
+	// }
 }
 /*******************************************************
-<<<<<<< HEAD
         Création du Custom Post(Evenement).
 *******************************************************/
 add_filter('manage_wpscevents_posts_columns', 'events_add_column');
@@ -315,10 +235,6 @@ function events_add_column($defaults) {
 	return $new;
 }
 
-=======
-				Création du Custom Post(Evenement).
-*******************************************************/
->>>>>>> Test fonction ajax
 add_action('init', 'events');
 function events(){
 	$labels = array(
@@ -475,11 +391,7 @@ function initialize_events_meta_boxes() {
 		include get_template_directory().'/init.php';
 }
 /*************************************************
-<<<<<<< HEAD
         Création des Réseaux Impliqués
-=======
-			Création des Custom post(Partenaires).
->>>>>>> Test fonction ajax
 *************************************************/
 add_action('init', 'partners');
 function partners(){
@@ -540,12 +452,7 @@ function partners_metaboxes( array $meta_boxes ) {
 	return $meta_boxes;
 }
 /*************************************************
-<<<<<<< HEAD
         Ressources (Custom Type Englobant)
-=======
-			Création des Custom post Ressources
-				(Custom Type Englobant).
->>>>>>> Test fonction ajax
 *************************************************/
 add_action('init', 'ressources');
 function ressources(){
@@ -585,11 +492,7 @@ function ressources(){
 	register_post_type('ressources',$args);
 }
 /*************************************************
-<<<<<<< HEAD
               Bloc Analyse
-=======
-							Bloc Analyse
->>>>>>> Test fonction ajax
 *************************************************/
 add_action('init', 'analyse');
 function analyse(){
@@ -684,11 +587,7 @@ function analyse_metaboxes( array $meta_boxes ) {
 	return $meta_boxes;
 }
 /*************************************************
-<<<<<<< HEAD
             Bloc Méthodologie
-=======
-						Bloc Méthodologie
->>>>>>> Test fonction ajax
 *************************************************/
 add_action('init', 'methodologie');
 function methodologie(){
@@ -782,11 +681,7 @@ function methodologie_metaboxes( array $meta_boxes ) {
 	return $meta_boxes;
 }
 /*************************************************
-<<<<<<< HEAD
               Bloc Témoignage
-=======
-						Bloc Témoignage
->>>>>>> Test fonction ajax
 *************************************************/
 add_action('init', 'temoignage');
 function temoignage(){
@@ -880,11 +775,7 @@ function temoignage_metaboxes( array $meta_boxes ) {
 	return $meta_boxes;
 }
 /*************************************************
-<<<<<<< HEAD
               Metabox post
-=======
-							Metabox post
->>>>>>> Test fonction ajax
 *************************************************/
 add_filter( 'meta_boxes', 'post_metaboxes' );
 function post_metaboxes( array $meta_boxes ) {
@@ -909,7 +800,6 @@ function post_metaboxes( array $meta_boxes ) {
 	// Add other metaboxes as needed
 	return $meta_boxes;
 }
-<<<<<<< HEAD
 
 function custom_js_to_head() {
     ?>
@@ -937,115 +827,5 @@ function custom_js_to_head() {
     });
     </script>
     <?php
-=======
-/*************************************************
-						AutoCompletion
-*************************************************/
-function auto_search(){
-	global $wpdb;
-	// the query
-	$args = array('posts_per_page' => 100, 'order'=> 'ASC', 'orderby' => 'date');
-	$the_query = new WP_Query( $args );
-	echo "<script type='text/javascript'>var availableTags = [";
-	$url_pattern = '/((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/';
-	$url_replace = ' ';
-	if ( $the_query->have_posts() ) : ?>
-		<?php $completion = '';
-		while ( $the_query->have_posts() ) : $the_query->the_post();
-			$post_id =  get_the_ID();
-			$sqlTitleContent = 'SELECT post_content, post_title
-			FROM  wp_posts
-			WHERE  ID ="'.$post_id.'"';
-			$result = $wpdb->get_results($sqlTitleContent);
-			$completion .= strip_tags(strtolower($result[0]->post_title));
-			$completion .= strip_tags(strtolower($result[0]->post_content));
-			$completion = preg_replace('@<a[^>]*?>.*?</a>@si', '', $completion);
-		endwhile;
-		$completionFormat = str_replace( array( '?', ',', '.', ':', '!', '"','/>','&nbsp;'), ' ', $completion );
-		$completion = str_replace( array('É'), 'é', $completionFormat );
-		$completionFormat = str_replace(array("\r\n", "\r", "\n"), "<br />", $completion);
-		$tablComplet = explode(" ", $completionFormat);
-		$cleanTab = array_unique($tablComplet);
-		$tablComplet = [];
-		for( $i=0; $i<count($cleanTab); $i++ ){
-			if(isset($cleanTab[$i]) && !preg_match("/[0-9]{1,2}$/", $cleanTab[$i]) && strlen($cleanTab[$i]) > 3 ){
-				echo '"'.rtrim(strtolower($cleanTab[$i])).'",';
-			}
-		}
-		wp_reset_postdata();
-	endif;
-	echo "];";
-	echo "jQuery('#s').autocomplete({source:availableTags});";
-	echo "jQuery('#s01').autocomplete({source:availableTags});";
-	echo "</script>";
->>>>>>> Test fonction ajax
 }
-add_action( 'wp_footer', 'auto_search' );
-/*************************************************
-								Galerie
-*************************************************/
-function gallery_func(){
-	$argsThumb = array(
-		'order'          => 'ASC',
-		'post_type'      => 'attachment',
-		'post_mime_type' => 'image',
-		'post_status'    => null
-	);
-	$attachments = get_posts($argsThumb);
-	if ($attachments) {
-		echo '<ul class="grid-thumb">';
-		foreach ($attachments as $attachment) {
-			//echo apply_filters('the_title', $attachment->post_title);
-			echo '<li><img src="'.wp_get_attachment_url($attachment->ID, 'testsize', false, false).'" /></li>';
-		}
-		echo '</ul>';
-	}
-}
-add_shortcode('my_gallery','gallery_func');
-/*************************************************
-						Carte OpenStreetMap
-*************************************************/
-function street_map(){
-	echo '<iframe width="100%" height="300px" frameBorder="0" src="http://umap.openstreetmap.fr/en/map/carte-du-pouvoir-dagir_63384?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="http://umap.openstreetmap.fr/en/map/carte-du-pouvoir-dagir_63384">Voir en plein écran</a></p>';
-}
-add_shortcode('map','street_map');
-/*************************************************
-							Aside Widget
-*************************************************/
-if( function_exists('register_sidebar')){
-	$args = array(
-		'name'          => __( 'Aside', 'theme_text_domain' ),
-		'id'            => 'aside',
-		'description'   => '',
-		'class'         => '',
-		'before_widget' => '<aside">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widgettitle">',
-		'after_title'   => '</h2>' );
-		register_sidebar($args);
-	}
-/*************************************************
-	Trier les articles selon une taxonomie
-*************************************************/
-	//allows queries to be sorted by taxonomy term name
-/*add_filter('posts_clauses', 'posts_clauses_with_tax', 10, 2);
-function posts_clauses_with_tax( $clauses, $wp_query ) {
-	global $wpdb;
-	//array of sortable taxonomies
-	$taxonomies = array('wpsclocation');
-	if (isset($wp_query->query['orderby']) && in_array($wp_query->query['orderby'], $taxonomies)) {
-	$clauses['join'] .= "
-		LEFT OUTER JOIN {$wpdb->term_relationships} AS rel2 ON {$wpdb->posts}.ID = rel2.object_id
-		LEFT OUTER JOIN {$wpdb->term_taxonomy} AS tax2 ON rel2.term_taxonomy_id = tax2.term_taxonomy_id
-		LEFT OUTER JOIN {$wpdb->terms} USING (term_id)
-	";
-	$clauses['where'] .= " AND (taxonomy = '{$wp_query->query['orderby']}' OR taxonomy IS NULL)";
-	$clauses['groupby'] = "rel2.object_id";
-	$clauses['orderby'] = "GROUP_CONCAT({$wpdb->terms}.name ORDER BY name ASC) ";
-	$clauses['orderby'] .= ( 'ASC' == strtoupper( $wp_query->get('order') ) ) ? 'ASC' : 'DESC';
-	}
-	return $clauses;
-}*/
-/*************************************************
-
-*************************************************/
+add_action('admin_head', 'custom_js_to_head');
